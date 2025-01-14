cd1e17935c5390feff5bbcc4213eaec32d95f0da
Saani <saabb743@learner.dvusd.org>
2025-01-14 08:57:50 -0700

    d

---
+++ Binary/Binary/Form1.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml.Schema;
+
+namespace Binary
+{
+    public partial class Total : Form
+    {
+        int total = 0;
+        int[] bits = new int[8];
+        public Total()
+        {
+            InitializeComponent();
+        }
+
+        private void Calc()
+        {
+            total = 0;
+            if (bits[0] != 0) total += 1;
+            if (bits[1] != 0) total += 2;
+            if (bits[2] != 0) total += 4;
+            if (bits[3] != 0) total += 8;
+            if (bits[4] != 0) total += 16;
+            if (bits[5] != 0) total += 32;
+            if (bits[6] != 0) total += 64;
+            if (bits[7] != 0) total += 128;
+            TotalLabel.Text = total.ToString();
+        }
+        private void Form1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void bit8_TextChanged(object sender, EventArgs e)
+        {
+            //bit1
+            if (bit1.Text == "1")
+            {
+                bits[0] = 1;
+            }
+            else
+            {
+                bits[0] = 0;
+            }
+            //bit2
+            if (bit2.Text == "1")
+            {
+                bits[0] = 1;
+            }
+            else
+            {
+                bits[2] = 0;
+            }
+
+            //bit4
+            if (bit4.Text == "1")
+                bits[3] = 1;
+            else
+                bits[3] = 0;
+            //bit5
+
+            Calc();
+        }
+    }
+}
+
+        
---
+++ Binary/Form1.Designer.cs
@@ -0,0 +1,150 @@
+﻿namespace Binary
+{
+    partial class Total
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.bit5 = new System.Windows.Forms.TextBox();
+            this.bit4 = new System.Windows.Forms.TextBox();
+            this.bit3 = new System.Windows.Forms.TextBox();
+            this.bit2 = new System.Windows.Forms.TextBox();
+            this.bit1 = new System.Windows.Forms.TextBox();
+            this.bit6 = new System.Windows.Forms.TextBox();
+            this.bit7 = new System.Windows.Forms.TextBox();
+            this.bit8 = new System.Windows.Forms.TextBox();
+            this.TotalLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // bit5
+            // 
+            this.bit5.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.bit5.Location = new System.Drawing.Point(297, 190);
+            this.bit5.Name = "bit5";
+            this.bit5.Size = new System.Drawing.Size(34, 27);
+            this.bit5.TabIndex = 0;
+            // 
+            // bit4
+            // 
+            this.bit4.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.bit4.Location = new System.Drawing.Point(417, 125);
+            this.bit4.Name = "bit4";
+            this.bit4.Size = new System.Drawing.Size(34, 27);
+            this.bit4.TabIndex = 1;
+            // 
+            // bit3
+            // 
+            this.bit3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.bit3.Location = new System.Drawing.Point(377, 125);
+            this.bit3.Name = "bit3";
+            this.bit3.Size = new System.Drawing.Size(34, 27);
+            this.bit3.TabIndex = 2;
+            // 
+            // bit2
+            // 
+            this.bit2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.bit2.Location = new System.Drawing.Point(337, 125);
+            this.bit2.Name = "bit2";
+            this.bit2.Size = new System.Drawing.Size(34, 27);
+            this.bit2.TabIndex = 3;
+            // 
+            // bit1
+            // 
+            this.bit1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.bit1.Location = new System.Drawing.Point(297, 125);
+            this.bit1.Name = "bit1";
+            this.bit1.Size = new System.Drawing.Size(34, 27);
+            this.bit1.TabIndex = 4;
+            // 
+            // bit6
+            // 
+            this.bit6.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.bit6.Location = new System.Drawing.Point(337, 190);
+            this.bit6.Name = "bit6";
+            this.bit6.Size = new System.Drawing.Size(34, 27);
+            this.bit6.TabIndex = 5;
+            // 
+            // bit7
+            // 
+            this.bit7.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.bit7.Location = new System.Drawing.Point(377, 190);
+            this.bit7.Name = "bit7";
+            this.bit7.Size = new System.Drawing.Size(34, 27);
+            this.bit7.TabIndex = 6;
+            // 
+            // bit8
+            // 
+            this.bit8.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.bit8.Location = new System.Drawing.Point(417, 190);
+            this.bit8.Name = "bit8";
+            this.bit8.Size = new System.Drawing.Size(34, 27);
+            this.bit8.TabIndex = 7;
+            this.bit8.TextChanged += new System.EventHandler(this.bit8_TextChanged);
+            // 
+            // TotalLabel
+            // 
+            this.TotalLabel.AutoSize = true;
+            this.TotalLabel.Location = new System.Drawing.Point(582, 337);
+            this.TotalLabel.Name = "TotalLabel";
+            this.TotalLabel.Size = new System.Drawing.Size(0, 16);
+            this.TotalLabel.TabIndex = 8;
+            // 
+            // Total
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.TotalLabel);
+            this.Controls.Add(this.bit8);
+            this.Controls.Add(this.bit7);
+            this.Controls.Add(this.bit6);
+            this.Controls.Add(this.bit1);
+            this.Controls.Add(this.bit2);
+            this.Controls.Add(this.bit3);
+            this.Controls.Add(this.bit4);
+            this.Controls.Add(this.bit5);
+            this.Name = "Total";
+            this.Text = "Form1";
+            this.TextChanged += new System.EventHandler(this.Form1_TextChanged);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox bit5;
+        private System.Windows.Forms.TextBox bit4;
+        private System.Windows.Forms.TextBox bit3;
+        private System.Windows.Forms.TextBox bit2;
+        private System.Windows.Forms.TextBox bit1;
+        private System.Windows.Forms.TextBox bit6;
+        private System.Windows.Forms.TextBox bit7;
+        private System.Windows.Forms.TextBox bit8;
+        private System.Windows.Forms.Label TotalLabel;
+    }
+}
+
